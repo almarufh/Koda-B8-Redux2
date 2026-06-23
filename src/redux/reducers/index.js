0a857e864a0747509c3bb104b4bb058f82b5e086
almarufh <hidayatmaruf99@gmail.com>
2026-06-23 17:01:39 +0800

    feat: use persistReducer to use localStorage

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,17 @@
 import { combineReducers } from "@reduxjs/toolkit";
+import persistReducer from "redux-persist/es/persistReducer";
+import storage from "redux-persist/es/storage"
 
 import todos from "./todos.js"
 
+const persistConfigTodos = {
+    key: "data",
+    storage
+
+};
+
 const reducer = combineReducers({
-    todos
+    todos: persistReducer(persistConfigTodos, todos)
 })
 
 export default reducer;
